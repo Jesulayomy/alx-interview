45060a3b88504fe9ac207b031af55caa43b27b8e
Jesulayomy <jesulayomi081@gmail.com>
2023-08-17 17:07:58 +0000

    Logs: Parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" This module parses a log (or log file) and returns data from the lines """
+import re
+import signal
+import sys
+
+
+def sigint_handler(signal, frame):
+    """ Handles C-c signal from stdin """
+    log(message)
+    sys.exit(0)
+
+
+def log(message: dict) -> None:
+    """ Logs the status to the screen """
+
+    for key, value in message.items():
+        if value != 0:
+            print(f"{key}: {value}")
+
+
+def processor(counter):
+    """ Runs the program to log parse"""
+    for line in sys.stdin:
+        if counter == 10:
+            log(message)
+            counter = 0
+        line = line.strip()
+        valid = re.match(regEX, line)
+        counter += 1
+        if not valid:
+            continue
+        valid = regEX.search(line)
+        res = list(valid.groups())
+        message["File size"] += int(res[-1])
+        message[int(res[-2])] += 1
+
+
+if __name__ == "__main__":
+    message = {
+        "File size": 0, 200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0}
+    counter = 0
+    regEX = re.compile(r"""([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]
+\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9])\ \-\ \[([0-9]{4}\-[0-1]{1}
+[0-9]{1}\-[0-3]{1}[0-9]{1}\ [0-2]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\:
+[0-5]{1}[0-9]{1}\.[0-9]{6})\]\ (\"GET\ \/projects\/260\ HTTP\/1\.1\")
+\ (200|301|400|401|403|404|405|500)\ ([1-9][0-9]*)""", re.X)
+    signal.signal(signal.SIGINT, sigint_handler)
+    processor(counter)
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,3 @@
+# Log Parsing backend practice
+
+- Written with python
