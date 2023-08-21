72d738648e874bbe6b0ba46dc23ff57f91661162
Jesulayomy <jesulayomi081@gmail.com>
2023-08-21 03:34:46 +0000

    Modified to match non ip and non status code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """ This module parses a log (or log file) and returns data from the lines """
 import re
-import sys
-
-
-# def sigint_handler(signal, frame):
-#     """ Handles C-c signal from stdin """
-#     log(message)
-#     sys.exit(0)
 
 
 def log(message: dict) -> None:
     """ Logs the status to the screen """
 
     for key, value in message.items():
-        if value != 0:
-            print(f"{key}: {value}")
+        if value != 0 or key == "File size":
+            print(f"{key}: {value}", flush=True)
 
 
 def processor(counter):
     """ Runs the program to log parse"""
 
     try:
-        for line in sys.stdin:
+        # for line in sys.stdin:
+        while True:
+            line = input()
             if counter == 10:
                 log(message)
                 counter = 0
-            line = line.strip()
             valid = re.match(regEX, line)
             counter += 1
             if not valid:
@@ -34,10 +28,12 @@ def processor(counter):
             valid = regEX.search(line)
             res = list(valid.groups())
             message["File size"] += int(res[-1])
-            message[int(res[-2])] += 1
-    except KeyboardInterrupt:
+            try:
+                message[int(res[-2])] += 1
+            except ValueError:
+                pass
+    except (KeyboardInterrupt, EOFError):
         log(message)
-        # sys.exit(0)
 
 
 if __name__ == "__main__":
@@ -45,9 +41,11 @@ if __name__ == "__main__":
         "File size": 0, 200: 0, 301: 0, 400: 0, 401: 0,
         403: 0, 404: 0, 405: 0, 500: 0}
     counter = 0
-    regEX = re.compile(r"""([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]
-\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9])\ \-\ \[([0-9]{4}\-[0-1]{1}
+    regEX = re.compile(r"""([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?
+[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]|[^\ ]*[^\ ])\ ?\-\ ?\[([0-9]{4}\-[0-1]{1}
 [0-9]{1}\-[0-3]{1}[0-9]{1}\ [0-2]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\:
 [0-5]{1}[0-9]{1}\.[0-9]{6})\]\ (\"GET\ \/projects\/260\ HTTP\/1\.1\")
-\ (200|301|400|401|403|404|405|500)\ ([1-9][0-9]*)""", re.X)
+\ ([^\ ]*[^\ ])\ ([1-9][0-9]*)""", re.X)
     processor(counter)
+# \ (200|301|400|401|403|404|405|500|*)\ ([1-9][0-9]*)""", re.X)
+# changed to allow anything
