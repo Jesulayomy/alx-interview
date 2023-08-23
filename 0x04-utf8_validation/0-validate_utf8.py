c98e851fc1ddf0e6e00c826532141f8bca8372d6
Jesulayomy <jesulayomi081@gmail.com>
2023-08-23 14:58:02 +0000

    Validating data

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" This module validates a stream of utf8 data """
+from typing import List
+
+
+def validUTF8(data: List[int]):
+    """
+        Confirms if a set of data is a valid utf8 encoding
+        Returns True if valid or false if nolt valid
+    """
+
+    for byte in data:
+        if byte > 255:
+            return False
+
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,5 @@
+# UTF-8 Validation
+
+- Validating utf-8 data
+- Changing data from utf8 to its binary encoding
+- Checking if data is valid utf-8
