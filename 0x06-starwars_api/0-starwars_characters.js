2ea63428371e889992243f88db689e501b449564
Jesulayomy <jesulayomi081@gmail.com>
2023-09-07 08:36:07 +0000

    [Correction]: using promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-request.get(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-  const characters = JSON.parse(body).characters;
-
-  for (const person of characters) {
-    request.get(person, (err, res, body) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(JSON.parse(body).name);
-    });
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      console.log(res);
+    }
   }
 });
